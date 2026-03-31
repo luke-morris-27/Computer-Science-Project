2733a4be6d3df111c7db77e4a3010af432b0ca02
luke-morris-27 <lukemorristx@outlook.com>
2026-03-30 19:22:46 -0500

    Update UiReportingService.java

--- src/main/java/ui/UiReportingService.java
+++ src/main/java/ui/UiReportingService.java
@@ -10,6 +10,8 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface UiReportingService {
+    List<WordReportView> listWords(WordReportSort sort, int limit) throws SQLException;
+    
     List<WordReportView> listWords(WordReportSort sort, int limit, String searchText) throws SQLException;
 
     List<String> listGeneratedSentences(boolean onlyDuplicates, int limit) throws SQLException;
