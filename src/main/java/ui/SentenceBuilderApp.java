2301a9271c0ff6e88d8c09de189df49f9928d3e9
luke-morris-27 <lukemorristx@outlook.com>
2026-04-13 18:59:07 -0500

    Update SentenceBuilderApp.java

--- src/main/java/ui/SentenceBuilderApp.java
+++ src/main/java/ui/SentenceBuilderApp.java
@@ -663,7 +663,7 @@ public class SentenceBuilderApp extends Application {
         controls.add(new Label("Search"), 0, 2);
         controls.add(reportSearchField, 1, 2, 3, 1);
         controls.add(new Label("Second word"), 3, 2);
-        controls.add(reportSecondWordField, 1, 2);
+        controls.add(reportSecondWordField, 3, 2);
 
         VBox content = new VBox(14,
             titledLabel("Reports"),
