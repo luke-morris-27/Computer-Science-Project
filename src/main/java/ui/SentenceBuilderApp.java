58cbfabf205f768e36863d1c4a21edab371eadbf
luke-morris-27 <lukemorristx@outlook.com>
2026-04-14 09:27:26 -0500

    Reorder column addition based on second word field

--- src/main/java/ui/SentenceBuilderApp.java
+++ src/main/java/ui/SentenceBuilderApp.java
@@ -711,12 +711,12 @@ public class SentenceBuilderApp extends Application {
         new ReadOnlyObjectWrapper<>(cell.getValue().precedesCount()));
         precedesColumn.setPrefWidth(100);
 
-        if (!reportSecondWordField.getText().isBlank()) {
-        table.getColumns().addAll(followsColumn, precedesColumn);
-        }
-
         table.getColumns().addAll(wordColumn, totalColumn, startColumn, endColumn);
         return table;
+        
+        if (!reportSecondWordField.getText().isBlank()) {
+            table.getColumns().addAll(followsColumn, precedesColumn);
+        }
     }
 
     private VBox createActivityLogPane() {
